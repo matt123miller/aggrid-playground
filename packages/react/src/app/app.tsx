24bd16f87bd67b5ecc419431624193afd39d58ae
Matt Miller <matt@m11r.dev>
2023-05-28 22:40:43 +0100

    playing with aggrid data patterns

--- packages/react/src/app/app.tsx
+++ packages/react/src/app/app.tsx
@@ -1,14 +1,106 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
+import {
+  GridApi,
+  CellClickedEvent,
+  ColDef,
+  GridOptions,
+} from 'ag-grid-community';
+import { AgGridReact } from 'ag-grid-react';
+import { useState, useRef, useCallback, useMemo, useEffect } from 'react';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
-import NxWelcome from './nx-welcome';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 
-export function App() {
+const App = () => {
+  const gridRef = useRef(null); // Optional - for accessing Grid's API
+
+  const createUserMutation = useMutation(
+    () => {
+      return fetch('http://localhost:4100/api/users', { method: 'POST' }).then(
+        (res) => res.json()
+      );
+    },
+    {
+      onSuccess: () => {
+        // result.refetch();
+        (gridRef.current?.api as GridApi).refreshInfiniteCache();
+      },
+    }
+  );
+
+  const deleteUserMutation = useMutation(
+    () => {
+      return fetch('http://localhost:4100/api/users/deleteRandom', {
+        method: 'DELETE',
+      }).then((res) => res.json());
+    },
+    {
+      onSuccess: () => {
+        (gridRef.current?.api as GridApi).refreshInfiniteCache();
+      },
+    }
+  );
+
+  // const { data, isLoading } = result;
+
+  const gridOptions: GridOptions = {
+    rowModelType: 'infinite',
+    getRowId: (row) => row.userId,
+    datasource: {
+      getRows: async (params) => {
+        console.log(params);
+
+        const results = await fetch('http://localhost:4100/api/users');
+        const json = await results.json();
+        params.successCallback(json, json.length);
+      },
+    },
+  };
+
+  // Each Column Definition results in one Column.
+  const columnDefs: ColDef[] = [
+    { field: 'userId' },
+    { field: 'username', filter: true },
+    { field: 'email', filter: true },
+    { field: 'birthdate', filter: 'agDateColumnFilter' },
+  ];
+
+  // DefaultColDef sets props common to all Columns
+  const defaultColDef = {
+    sortable: true,
+  };
+
+  // Example of consuming Grid Event
+  const cellClickedListener = useCallback((event: CellClickedEvent) => {
+    console.log('cellClicked', event);
+  }, []);
+
+  // Example using Grid's API
+  const createListener = useCallback((_) => {
+    createUserMutation.mutate();
+  }, []);
+
+  const deleteListener = () => {
+    deleteUserMutation.mutate();
+  };
+
   return (
-    <div>
-      <NxWelcome title="react" />
+    <div className="ag-theme-alpine" style={{ height: 1000, width: 1000 }}>
+      <button onClick={createListener}>Add user</button>
+      <button onClick={deleteListener}>Delete random user</button>
+      <AgGridReact
+        ref={gridRef} // Ref for accessing Grid's API
+        gridOptions={gridOptions}
+        columnDefs={columnDefs} // Column Defs for Columns
+        defaultColDef={defaultColDef} // Default Column Properties
+        animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+        rowSelection="multiple" // Options - allows click selection of rows
+        onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+      />
     </div>
   );
-}
+};
 
 export default App;
